d3737e8348bc3b2b26ab26acdac165505f0d99a5
vigneswarimurugaiyan <31652652+vigneswarimurugaiyan@users.noreply.github.com>
2017-12-29 14:58:56 +0530

    Add files via upload

---
+++ SocialNetworkBackEnd/src/main/java/com/maven/socialappbackend/dao/userdao.java
@@ -0,0 +1,17 @@
+package com.maven.socialappbackend.dao;
+
+import java.util.List;
+
+import com.maven.socialappbackend.model.userdetail;
+public interface userdao {
+	
+public boolean adduser(userdetail user);
+public boolean updateuser(userdetail user);
+public boolean deleteuser(userdetail user);
+public List<userdetail> getalluser();
+public userdetail getuserbyid(int userId);
+public userdetail getuser(String userName);
+public boolean checklogin(userdetail user);
+public boolean changeonlinestatus(userdetail user);
+
+}
